2237ba30dee2987b4bf000de6f7c6a1b48dd5f1d
Joelle Marianek <joelle@LAPTOP-6D50GD4A>
2024-01-13 16:33:44 +0100

    feat: add burger menu functionality, after link click menu closes

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -4,4 +4,9 @@ const mobileMenu = document.querySelector('.mobile-menu')
 burger.addEventListener("click", () => {
     burger.classList.toggle('active');
     mobileMenu.classList.toggle('active');
-});
+})
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+    burger.classList.remove('active');
+    mobileMenu.classList.remove('active')
+}))
